e5c06f19bb93167e2a5b6ea0f14fca6980d2e09f
yu miura <curelemonade15@gmail.com>
2024-03-10 20:41:29 +0900

    feat: prefixを配列で定義して、printfで改行区切りで出力するようにした

--- before-select-prefix.sh
+++ before-select-prefix.sh
@@ -1,3 +1,18 @@
 #bin/sh
-arr="fix\nadd\nfeat\nrefactor\ndisable\ndocs\ntest\nchore"
-echo ${arr} | peco | xargs -o -IPREFIX git commit -m PREFIX: -e
+
+#TODO: tomlから選択できるprefixを選択できるようにする
+prefixes=(
+  "build"
+  "chore"
+  "ci"
+  "docs"
+  "feat"
+  "fix"
+  "perf"
+  "refactor"
+  "revert"
+  "style"
+  "test"
+)
+
+printf "%s\n" "${prefixes[@]}" | peco | xargs -o -IPREFIX git commit -m PREFIX: -e
